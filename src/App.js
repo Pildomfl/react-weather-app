23fb6e4a4fd4c7916a37b1a69fea7e07829bf567
Pildomfl <151389400+Pildomfl@users.noreply.github.com>
2024-02-01 13:05:18 +0100

    Added Github repo URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Pilar Dominguez and is {""}
+        <a href="https://github.com/Pildomfl/react-weather-app" target="_blank">
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
