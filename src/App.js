c41b064236d42b5a15df1aebbcab944270d69531
Pildomfl <151389400+Pildomfl@users.noreply.github.com>
2024-02-01 13:07:24 +0100

    Added "noreferrer"

--- src/App.js
+++ src/App.js
@@ -6,7 +6,11 @@ export default function App() {
       <h1>Weather App</h1>
       <footer>
         This project was coded by Pilar Dominguez and is {""}
-        <a href="https://github.com/Pildomfl/react-weather-app" target="_blank">
+        <a
+          href="https://github.com/Pildomfl/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
           open-sourced on Github
         </a>
       </footer>
